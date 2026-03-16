c5701be7618f20ceb2deb28f2ed7febeab646478
qiuchun <362460803@qq.com>
2026-03-16 15:45:22 +0800

    finish the work

--- webdriver.py
+++ webdriver.py
@@ -1,11 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.common import exceptions
+from selenium.webdriver.remote.webdriver import WebElement
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import re
 import os
+import random
 from bs4 import BeautifulSoup   #网页解析，数据获取
+from bs4.element import Tag,PageElement
 from docx import Document
 import requests
+from enum import Enum
+
+class State(Enum):
+    Init = 0
+    Wait = 1
+    GetUrl = 2
+    ParseHtml = 3
+    OverAutoer = 4
 
 findTitle = re.compile(r'<h1>(.*?)</h1>',re.S)
 findEnd = re.compile(r'<div>(.*?)</div>',re.S)
@@ -15,10 +30,21 @@ def main():
     #word
     Doc = Document()
 
-    # 设置Chrome选项，模拟Chrome浏览器
+    """创建基础驱动"""
     chrome_options = Options()
-    chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
-    #chrome_options.add_argument("--headless")  # 无头模式，不显示浏览器界面
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+    
+    # 禁用自动化特征
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option('useAutomationExtension', False)
+
 
     # 初始化Chrome驱动
     driver = webdriver.Chrome(options=chrome_options)
@@ -27,38 +53,78 @@ def main():
     current_path = os.path.dirname(os.path.realpath(__file__))
     savepath = current_path + "/fuhuoquanrenlei.docx"
     print('save path', savepath)
-    url = 'https://twkan.com/txt/73947/45140676'
+    url = 'https://www.69shuba.com/txt/81790/39061006'
 
     link = ""
-    while(link!="https://twkan.com/txt/73947/end.html"):
-        try:
-            driver.get(url)
-            #time.sleep(1)  # 等待页面加载，根据需要调整时间
-            page_source = driver.page_source  # 获取页面源代码
-            
-            soup = BeautifulSoup(page_source, 'html5lib')
-            items = soup.find_all('div',attrs = {'class':'txtnav'})
-            for item in items:
-                item = str(item)
-                title = re.findall(findTitle, item)[0]
-                print('title:',title)
-                Doc.add_heading(title, level = 0)
+    print('begin')
+    print('current url:',driver.current_url)
 
-            items = soup.find_all('div',attrs = {'id':'txtcontent0'})
-            for item in items:
-                for content in item.contents:
-                    if not content.name:
-                        Doc.add_paragraph(content.string)
-                    #print(content)
-            Doc.save(savepath)
-            #跳转到下一页
-            for item in soup.find_all('div', attrs = {'class':'page1'}):
-                item = str(item)
-                link = re.findall(findLink, item)[0]
-                print('link', link)
-                url = str(link)
+    
+    _state:State = State.Init
+    _check_button:WebElement = None
+    while(link!="https://www.69shuba.com/book/81790.htm"):
+        try:
+            print(_state)
+            if _state == State.Init:
+                driver.get(url)
+                #time.sleep(5)  # 等待页面加载，根据需要调整时间
+                page_source = driver.page_source  # 获取页面源代码
+                #print(page_source)
+                #//*[@id='CZUq4']/div/label/input
+                _state = State.Wait
+                    
+            elif _state == State.Wait:
+                
+                time.sleep(random.randint(6,9))
+                _check_button = driver.find_element(By.XPATH, '//*[@id="CZUq4"]/div/label/input')
+                if _check_button:
+                    print(_check_button)
+                    if not _check_button.is_selected():
+                        #_check_button.click()
+                        webdriver.ActionChains(driver).move_to_element(_check_button).click(_check_button).perform()
+                    _state = State.OverAutoer
+            elif _state == State.GetUrl:
+                pass
+            elif _state == State.OverAutoer:
+                time.sleep(1)
+                _state = State.ParseHtml
+                pass
+            elif _state == State.ParseHtml:
+                soup = BeautifulSoup(page_source, 'html5lib')
+                titleElems = soup.find_all('h1',attrs = {'class':'hide720'})
+                #标题
+                for elem in titleElems:
+                    title = elem.text
+                    print('title:',title)
+                    Doc.add_heading(title, level = 0)
+                        
+                        
+                #内容
+                txtElems = soup.find_all('div',attrs = {'class':'txtnav'})
+                for elem in txtElems:
+                    for item in elem.contents:
+                        if not item.name:
+                            Doc.add_paragraph(item.text)
+                        #print(content)
+                #保存word
+                Doc.save(savepath)
+                #跳转到下一页
+                for item in soup.find_all('div', attrs = {'class':'page1'}):
+                    item = str(item)
+                    link = re.findall(findLink, item)[0]
+                    print('link', link)
+                    url = str(link)
+            
+            
         except requests.exceptions.RequestException as e:
             print(e)
+        except exceptions.NoSuchElementException as e:
+            print(e)
+            _state = State.Wait
+            continue
+        except exceptions.ElementNotInteractableException as e:
+            print("element is not interactable")
+            _state =State.Wait
 
 
     driver.quit()  # 关闭浏览器
